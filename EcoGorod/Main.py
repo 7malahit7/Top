4e244735c5f402394db57ae739252752958c1341
Илья Лыфарь <60061637+7malahit7@users.noreply.github.com>
2023-11-24 12:35:37 +0300

    Update Main.py

--- EcoGorod/Main.py
+++ EcoGorod/Main.py
@@ -3,32 +3,28 @@ from kivy.core.window import Window
 from kivy.uix.screenmanager import ScreenManager, Screen
 from kivy.lang import Builder
 from kivy.config import Config
+from kivy.core.window import Window
 
-Config.set('graphics','height','1920')
-Config.set('graphics','width','1080')
 Config.set('graphics','resizeable',False)
 Config.write()
 
 class Menu(Screen):
     pass
-
 class UserScreen1(Screen):
     pass
-
-
-class VolonterScreen1(Screen):
+class UserScreen2(Screen):
     pass
-
 class WindowManager(ScreenManager):
     pass
-
-
-class ecogorodApp(App):
+class VolonterScreen1(Screen):
+    pass
+    
+class ecogorodApp(App):    
     Window.clearcolor = ('white') 
+    Window.size = (1080,1920)
     def build(self):                                      
-        return Builder.load_file('ecogorod.kv')                                                                                           
-        
+        return Builder.load_file('ecogorod.kv')   
+                                                                                                    
 if __name__ == '__main__':
-    print("Started")
     ecogorodApp().run()
     
