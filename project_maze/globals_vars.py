84223ec6a1c336ec566bc38abf609b4f4b16c3b6
Илья Лыфарь <60061637+7malahit7@users.noreply.github.com>
2024-04-11 13:30:13 +0300

    Update and rename globals.py to globals_vars.py

---
+++ project_maze/globals_vars.py
@@ -0,0 +1,19 @@
+import pygame
+import random
+pygame.init()
+clock = pygame.time.Clock()
+GAP = 50
+start_cell_x = 0
+start_cell_y = 0
+width = 1600
+height = 900
+Button_Size_W = 350
+Button_Size_H = 100
+BLACK = (0,0,0)
+WHITE = (255,255,255)
+TEXT_COLOR = (10, 191, 42)
+font = pygame.font.SysFont('Courier New',35,bold=True)
+font2 = pygame.font.SysFont("Courier New",50,bold=True)
+color1 = (31, 25, 24)
+color2 = (56, 46, 44)
+    
